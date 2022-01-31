b09755b023920a1dce8c9cfc43bcdd21bb7f57ab
Rajesh071 <rajeshsahani071@gmail.com>
2022-01-31 13:34:27 +0530

    Added code

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
 import "./styles.css";
-import React, { useRef } from 'react'
+import React, { useRef } from "react";
+
+const APIKEY = "OPzSZvJ9FbJSkWzJx2y0ao1pdW8aKVBg";
 export default function App() {
-  return (
-   <ReactForm/>
-  );
+  return <ReactForm />;
 }
 
-
 function ReactForm() {
-
   const nameRef = useRef(null);
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
   const handleSubmit = (e) => {};
 
   const handleReset = () => {
-    nameRef.current.value="";
-    passwordRef.current.value="";
+    nameRef.current.value = "";
+    passwordRef.current.value = "";
 
-    emailRef.current.value="";
-
-
+    emailRef.current.value = "";
   };
 
   const handleSearch = (e) => {
     // log your value here
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${e}`)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   };
 
-  function debounce (fn, time) {
-    let timeoutId
-    return wrapper
-    function wrapper (...args) {
+  function debounce(fn, time) {
+    let timeoutId;
+    return wrapper;
+    function wrapper(...args) {
       if (timeoutId) {
-        clearTimeout(timeoutId)
+        clearTimeout(timeoutId);
       }
       timeoutId = setTimeout(() => {
-        timeoutId = null
-        fn(...args)
-      }, time)
+        timeoutId = null;
+        fn(...args);
+      }, time);
     }
   }
   const debouncedSearch = debounce(handleSearch, 1000);
@@ -51,26 +50,35 @@ function ReactForm() {
         </label>
         <label>
           Email:
-          <input placeholder="email" type="text" ref={emailRef}/>
+          <input placeholder="email" type="text" ref={emailRef} />
         </label>
-
 
         <label>
           Password:
-          <input placeholder="password" type="text"  ref={passwordRef}/>
+          <input placeholder="password" type="text" ref={passwordRef} />
         </label>
         <hr />
-        <button onClick={()=>nameRef.current.focus()}>Focus Name Input</button>
-        <button onClick={()=>emailRef.current.focus()}>Focus Email Input</button>
-        <button onClick={()=>passwordRef.current.focus()}>Focus Password Input</button>
+        <button onClick={() => nameRef.current.focus()}>
+          Focus Name Input
+        </button>
+        <button onClick={() => emailRef.current.focus()}>
+          Focus Email Input
+        </button>
+        <button onClick={() => passwordRef.current.focus()}>
+          Focus Password Input
+        </button>
         <hr />
-        <button onClick={()=>{console.log(nameRef.current.value);
-        console.log(emailRef.current.value);
+        <button
+          onClick={() => {
+            console.log(nameRef.current.value);
+            console.log(emailRef.current.value);
 
-        console.log(passwordRef.current.value);
-
-        }}>Submit</button>
-        <button onClick={()=>handleReset()}>Reset</button>
+            console.log(passwordRef.current.value);
+          }}
+        >
+          Submit
+        </button>
+        <button onClick={() => handleReset()}>Reset</button>
       </div>
       <div>
         <hr />
