9cb018f1de72ac97d169e9d1a62b41449fabc772
Rajesh071 <rajeshsahani071@gmail.com>
2022-01-31 13:19:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import "./styles.css";
+import React, { useRef } from 'react'
+export default function App() {
+  return (
+   <ReactForm/>
+  );
+}
+
+
+function ReactForm() {
+
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const handleSubmit = (e) => {};
+
+  const handleReset = () => {
+    nameRef.current.value="";
+    passwordRef.current.value="";
+
+    emailRef.current.value="";
+
+
+  };
+
+  const handleSearch = (e) => {
+    // log your value here
+  };
+
+  function debounce (fn, time) {
+    let timeoutId
+    return wrapper
+    function wrapper (...args) {
+      if (timeoutId) {
+        clearTimeout(timeoutId)
+      }
+      timeoutId = setTimeout(() => {
+        timeoutId = null
+        fn(...args)
+      }, time)
+    }
+  }
+  const debouncedSearch = debounce(handleSearch, 1000);
+  return (
+    <React.Fragment>
+      <div>
+        <p>part 1</p>
+        <label>
+          Name:
+          <input placeholder="name" type="text" ref={nameRef} />
+        </label>
+        <label>
+          Email:
+          <input placeholder="email" type="text" ref={emailRef}/>
+        </label>
+
+
+        <label>
+          Password:
+          <input placeholder="password" type="text"  ref={passwordRef}/>
+        </label>
+        <hr />
+        <button onClick={()=>nameRef.current.focus()}>Focus Name Input</button>
+        <button onClick={()=>emailRef.current.focus()}>Focus Email Input</button>
+        <button onClick={()=>passwordRef.current.focus()}>Focus Password Input</button>
+        <hr />
+        <button onClick={()=>{console.log(nameRef.current.value);
+        console.log(emailRef.current.value);
+
+        console.log(passwordRef.current.value);
+
+        }}>Submit</button>
+        <button onClick={()=>handleReset()}>Reset</button>
+      </div>
+      <div>
+        <hr />
+        <p>part 2</p>
+        <label>
+          Search:
+          <input
+            placeholder="search with debounce"
+            type="text"
+            onChange={debouncedSearch}
+          />
+        </label>
+      </div>
+    </React.Fragment>
+  );
+}
